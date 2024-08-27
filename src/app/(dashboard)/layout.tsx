de52dc79afeea93385e86d9f4d00021540a9a7ea
samaila-bashir <developer.chatto@gmail.com>
2024-08-27 11:15:33 +0100

    implement left menu navigation

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -5,10 +9,17 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="h-screen flex">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        Left
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] overflow-scroll">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2 p-4"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SchoolMS</span>
+        </Link>
+        <Menu />
       </div>
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] overflow-scroll">
         Right
       </div>
     </div>
