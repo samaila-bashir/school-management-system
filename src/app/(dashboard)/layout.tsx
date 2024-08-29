ace0125dbb46ad14233ab5c950185cc8dae0a1ab
samaila-bashir <developer.chatto@gmail.com>
2024-08-29 13:05:49 +0100

    implement finance chart

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,18 +9,18 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen flex">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] ">
+    <div className='h-full flex'>
+      <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] '>
         <Link
-          href="/"
-          className="flex items-center justify-center lg:justify-start gap-2 p-4"
+          href='/'
+          className='flex items-center justify-center lg:justify-start gap-2 p-4'
         >
-          <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block">SchoolMS</span>
+          <Image src='/logo.png' alt='logo' width={32} height={32} />
+          <span className='hidden lg:block'>SchoolMS</span>
         </Link>
         <Menu />
       </div>
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-hidden'>
         <Navbar />
         {children}
       </div>
